bb66339382ac20c189f82e5cf69aa1c2ed19d032
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-09-16 16:06:37 +0530

    done

--- index.js
+++ index.js
@@ -7,11 +7,26 @@ const data = fs.readFileSync('data.json','utf-8')
 
 const server = http.createServer((req, res) => {
   console.log(req.url);
+
+  switch(req.url){
+    case '/':
+      res.setHeader("Content-Type", "text/html");
+      res.end(index);
+      
+    case '/api':
+      res.setHeader('Content-Type','application/json');
+      res.end(data)
+    default:
+      res.writeHead(404, 'NT FOUND');
+      res.end();
+  }
+
+
   console.log("server started");
-  res.setHeader("Dummy", "DummyValue");
-  // res.setHeader("Content-Type", "text/html");
-  res.setHeader('Content-Type','application/json')
-  res.end(data);
+  // res.setHeader("Dummy", "DummyValue");
+  // res.setHeader("Dummy", "DummyValue");
+
+
 });
 
 server.listen(8080);
